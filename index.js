4a5a74693eeb25011c24164c024b5e2d92cbdbc6
Jamile Boffo <97992778+JamileBoffo@users.noreply.github.com>
2022-04-18 22:29:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+
+const router = require('./src/routes/movie.route');
+
 const port = 3000;
+
 const app = express();
-const router = require('./src/routes/movies.route');
-app.use(express.urlencoded({extended: true}));
+
 app.use(express.json());
 app.use(cors());
 app.use('/movies', router);
+
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
-

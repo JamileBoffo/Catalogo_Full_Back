85bdee176cdbcea754a0743a8a943a0109411319
Jamile Boffo <97992778+JamileBoffo@users.noreply.github.com>
2022-04-24 13:47:11 -0300

    Update movie.service.js

--- src/services/movie.service.js
+++ src/services/movie.service.js
@@ -1,61 +1,33 @@
-const movies = [
-  {
-    id: 1,
-    titulo: 'A Batalha Esquecida',
-    sinopse:
-      'Durante a Segunda Guerra, na crucial Batalha do Rio Escalda, três vidas totalmente diferentes se cruzam e cada um vê o conflito sob perspectivas diferentes.',
-    img: './assets/img/abatalhaesquecida.jpg',
-    ano: 2020,
-  },
-  {
-    id: 2,
-    titulo: 'Alerta Vermelho',
-    sinopse:
-      'Um alerta vermelho da Interpol é emitido e o agente do FBI John Hartley assume o caso. Durante sua busca, ele se vê diante de um assalto ousado e é forçado a se aliar ao maior ladrão de arte da história, Nolan Booth, para capturar a ladra de arte mais procurada do mundo atualmente, Sarah Black.',
-    img: './assets/img/alertavermelho.jpg',
-    ano: 2021,
-  },
-  {
-    id: 3,
-    titulo: 'Amor e Monstros',
-    sinopse:
-      'Sete anos após sobreviver ao apocalipse dos monstros, o azarado Joel Dawson decide se arriscar e abandona o seu bunker para se reencontrar com a sua ex-namorada da escola, Aimee.',
-    img: './assets/img/amoremonstros.jpg',
-    ano: 2020,
-  },
-];
+const movie = require('../models/movie');
 
-const findAllMoviesService = () => {
+const findAllMoviesService = async () => {
+  const movies = await movie.find();
   return movies;
 };
 
-const findByIdMovieService = (idParam) => {
-  return movies.find((movie) => movie.id === idParam);
+const findByIdMovieService = async (idParam) => {
+  const movieid = await movie.findById(idParam);
+  return movieid;
 };
 
-const createMovieService = (newMovie) => {
-  const newId = movies.length + 1;
-  newMovie.id = newId;
-  movies.push(newMovie);
-  return newMovie;
+const createMovieService = async (newMovie) => {
+  const createdMovie = await movie.create(newMovie);
+  return createdMovie;
 };
 
-const updateMovieService = (id, movieEdited) => {
-  movieEdited['id'] = id;
-  const movieIndex = movies.findIndex((movie) => movie.id == id);
-  movies[movieIndex] = movieEdited;
-  return movieEdited;
+const updateMovieService = async (id, movieEdited) => {
+  const movieUpdate = await movie.findByIdAndUpdate(id, movieEdited);
+  return movieUpdate;
 };
 
-const deleteMovieService = (id) => {
-  const movieIndex = movies.findIndex((movie) => movie.id == id);
-  return movies.splice(movieIndex, 1);
+const deleteMovieService = async (id) => {
+  return await movie.findByIdAndDelete(id);
 };
 
 module.exports = {
-findAllMoviesService,
-findByIdMovieService,
-createMovieService,
-updateMovieService,
-deleteMovieService
+  findAllMoviesService,
+  findByIdMovieService,
+  createMovieService,
+  updateMovieService,
+  deleteMovieService,
 };
